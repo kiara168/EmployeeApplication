ffa4e5cc49cf85e1a15c7595962940b29280a23e
kiara168 <97761434+kiara168@users.noreply.github.com>
2022-03-07 15:00:02 -0500

    Add files via upload

---
+++ EmployeeApplicationProject/src/test/java/com/genspark/EmployeeApplicationProject/EmployeeApplicationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.EmployeeApplicationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
